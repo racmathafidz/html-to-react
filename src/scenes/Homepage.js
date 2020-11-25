2abfb245169b2ea16b7d4489195603db81fa2776
racmathafidz <rac.hafidz@gmail.com>
2020-11-25 23:45:40 +0700

    Adding Homepage Scene

---
+++ src/scenes/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'elements/Button'
+
+export default function Homepage() {
+    return (
+        <div>
+            <Button isPrimary isSmall>Sign Up</Button>
+        </div>
+    )
+}
